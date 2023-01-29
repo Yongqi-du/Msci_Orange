5d8d1712ccb9d2a299a2ef32905e0183b701470c
Guangzhong Chen <guangzhong0@gmail.com>
2023-01-29 15:52:42 +0000

    Create allocation-policy.py

---
+++ backend/allocation-policy.py
@@ -0,0 +1,27 @@
+'''
+Allocation Policy
+
+Applications
+ApplicationID | Band | Category | Bedroom | BandStartDate | Wait Time
+
+
+Property
+Bedroom | Category | ReleaseDate
+ReleaseDate
+    1. Do we assume properties open up after certain times
+    2. Can we get some tracking information on property availability
+
+Order applications by BandStartDate
+Initialize current_date to track allocation process
+while loop to (while current_date < latest date in data)
+    add applications who have the same BandStartDate as current_date to pool
+    sort applications by Category
+    Category is split by Band
+    Band is ordered by BandStartDate
+    bands are connected in order (1 to 5)
+    if current_date == Property.ReleaseDate
+        check category of property
+        find highest priority in fitting applications
+    increment application's waiting time
+    increment current_date
+'''
