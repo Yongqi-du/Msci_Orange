c69185828bb5c40bda402a29c332bcf37038d219
Guangzhong Chen <guangzhong0@gmail.com>
2023-01-31 13:26:13 +0000

    Initial Data Loading (#6)
    * Add data files and start ingest
    
    * Create test graph using matplotlib
    
    * Create allocation-policy.py
    
    * Update allocation-policy.py
    
    * Moved allocation-policy.py into data-manipulation-backend folder
    
    * Update allocation-policy.py
    
    * Update allocation-policy.py
    
    * Update allocation-policy.py
    
    * Really basic stuff with datetime
    
    ---------
    
    Co-authored-by: Fahim Mohammed <mohammedf2606@gmail.com>

---
+++ data-manipulation-backend/src/allocation-policy.py
@@ -0,0 +1,51 @@
+'''
+Allocation Policy
+
+Applications
+ApplicationID | Band | Category | Bedroom | BandStartDate | Wait Time
+
+
+Property
+PropertyID | Bedroom | Category | ReleaseDate
+ReleaseDate
+    1. Do we assume properties open up after certain times
+        Apparently council get access to homes every year (58 1 bed flats, 53 2 bed flats, etc)
+    2. Can we get some tracking information on property availability
+
+Order applications by BandStartDate
+Initialize current_date to track allocation process
+while loop to (while current_date < latest date in data)
+    add applications who have the same BandStartDate as current_date to pool
+    sort applications by Category
+    Category is split by Band
+    Band is ordered by BandStartDate
+    bands are connected in order (1 to 5)
+    if current_date == Property.ReleaseDate
+        check category of property
+        find highest priority in fitting applications
+    increment application's waiting time
+    increment current_date
+'''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+
+if __name__ == "__main__":
+    housing_stock = pd.read_excel("../data/HRA_stock.xlsx", engine="openpyxl")
+    housing_register = pd.read_excel("../data/RBK_Housing_Register.xlsx", engine="openpyxl")
+    # print(housing_stock)
+    print(housing_register)
+    housing_register_ordered_by_band_date = housing_register.sort_values(by="BandStartDate")
+    print(housing_register_ordered_by_band_date)
+    current_date = datetime.datetime(2014, 1, 1, 0, 0, 0)
+
+    last_row = housing_register_ordered_by_band_date.tail(1)
+    value = last_row['BandStartDate'].dt.to_pydatetime()
+    print(value)
+    print(value > current_date)
+    # value = str(value)
+    # value = value[:-3]
+    # date_object = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+
+    # print(date_object > current_date)
