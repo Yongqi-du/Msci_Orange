cce80b9fa0ddab67512b3c5a93a8d987b1b0b989
Guangzhong Chen <guangzhong0@gmail.com>
2023-02-02 16:02:59 +0000

    Added More buttons in top-navbar and functionality for all of them

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -5,7 +5,7 @@ import './style.css';
 
 import button_circle from './button-circle.png';
 
-function Navbar( {toggleMain, toggleUpload} ) {
+function Navbar( {toggleMain, toggleModel, toggleUpload, toggleExport, toggleUser, toggleSettings} ) {
   return (
     <div className='navbar'>
         <div className='logo-hamburger'>
@@ -15,34 +15,39 @@ function Navbar( {toggleMain, toggleUpload} ) {
           </div>
         </div>
         <div className="tabs-segmented">
-          <div className='tabs-segmented-item' onClick={toggleMain}>
+          <div className='first tabs-segmented-item' onClick={toggleMain}>
             <div className="first">
                 <div className='caption-3 valign-text-middle inter-normal-topaz-10-4px'>
                     Home
                 </div>
             </div>
           </div>
-          
+          <div className='tabs-segmented-item' onClick={toggleModel}>
+            <div className='caption-3 valign-text-middle inter-normal-topaz-10-4px'>
+              Model Allocation
+            </div>
+          </div>
           <div className='tabs-segmented-item' onClick={toggleUpload}>
             <div className='caption-3 valign-text-middle inter-normal-topaz-10-4px'>
               Load Data
             </div>
           </div>
-          <div className='tabs-segmented-item'>
+          <div className='tabs-segmented-item' onClick={toggleExport}>
             <div className='caption-3 valign-text-middle inter-normal-topaz-10-4px'>
               Export
             </div>
           </div>
+          
         </div>
         <div className='right-userlist'>
           <div className='list'>
-            <div className='list-master-2'>
+            <div className='list-master-2' onClick={toggleUser}>
               <div className="caption-5 valign-text-middle inter-normal-eerie-black-10-4px">
                 David Hill
               </div>
             </div>
           </div>
-          <div className='button-default'>
+          <div className='button-default' onClick={toggleSettings}>
             <div className='body'>
               <div className='caption-6 valign-text-middle'>
                 Settings
