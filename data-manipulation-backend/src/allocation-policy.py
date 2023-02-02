8a8dfc3ae82bcbd871729741ae3c90f99e93ebeb
Guangzhong Chen <guangzhong0@gmail.com>
2023-02-02 19:39:07 +0000

    Added code to split data

--- data-manipulation-backend/src/allocation-policy.py
+++ data-manipulation-backend/src/allocation-policy.py
@@ -40,21 +40,306 @@ if __name__ == "__main__":
     housing_register_ordered_by_band_date = housing_register.sort_values(by="BandStartDate")
 
     # Initialize model (date of housing property)
-    current_date = datetime.datetime(2014, 1, 1, 0, 0, 0)
+    current_date = datetime.datetime(2022, 7, 31, 0, 0, 0)
 
     # Model end date
     last_row = housing_register_ordered_by_band_date.tail(1)
-    final_date = last_row['BandStartDate'].dt.to_pydatetime()
+    # final_date = last_row['BandStartDate'].dt.to_pydatetime()
+    final_date = datetime.datetime(2022, 10, 1, 0, 0, 0)
 
     # Initialize lists of categories to use for sorting priority
-    Homeless = []
-    FirstTimeApplicant = []
+    ArmedForcesPersonnel = [[], [], [], [], []]
+    ArmedForcesVeterans = [[], [], [], [], []]
+    FirstTimeApplicant = [[], [], [], [], []]
+    GypsyAndTravellers=[[], [], [], [], []]
+    homescheme = [[], [], [], [], []]
+    Homeless = [[], [], [], [], []]
+    MobilitySchemes = [[], [], [], [], []]
+    PanelMoves = [[], [], [], [], []]
+    PermanentDecants = [[], [], [], [], []]
+    Sheltered = [[], [], [], [], []]
+    SocialServicesQuota = [[], [], [], [], []]
+    SpareRoomDownsizer = [[], [], [], [], []]
+    StaffRehousing = [[], [], [], [], []]
+    TemporaryDecants = [[], [], [], [], []]
+    TenantFinderServicePrevention = [[], [], [], [], []]
+    Transfer = [[], [], [], [], []]
+    UnderOccupier = [[], [], [], [], []]
 
+    '''
+    Run model until date is past final_date
+    Final_date can be:
+        1. latest date in data
+        2. target date to run modelling
+    '''
+    index = 0
+    maxIndex = len(housing_register_ordered_by_band_date)-1
+    # print(maxIndex)
     while current_date < final_date:
-        index = 0
-        row = housing_register_ordered_by_band_date.iloc[index]
-        print(row)
-        index += 1
+        '''
+        Increment index until row's BandStartDate is larger than current date
+        '''
+        current_application = housing_register_ordered_by_band_date.iloc[index]
+        current_application_date = current_application["BandStartDate"]
+        # print(current_application_date)
+
+        while current_application_date < current_date:
+            if index < maxIndex:
+                index += 1
+                # print(index)
+                current_application = housing_register_ordered_by_band_date.iloc[index]
+                current_application_date = current_application["BandStartDate"]
+                current_application_id = current_application["ApplicationId"]
+                current_application_band = current_application['Band']
+                
+                '''
+                Assign application to corresponding list based on AppCategory
+                '''
+                current_application_category = current_application["AppCategory"]
+                if current_application_category == "Armed Forces Personnel":
+                    if current_application_band == "Band 1":
+                        ArmedForcesPersonnel[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        ArmedForcesPersonnel[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        ArmedForcesPersonnel[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        ArmedForcesPersonnel[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        ArmedForcesPersonnel[4].append(current_application_id)
+
+                elif current_application_category == "Armed Forces Veterans":
+                    if current_application_band == "Band 1":
+                        ArmedForcesVeterans[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        ArmedForcesVeterans[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        ArmedForcesVeterans[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        ArmedForcesVeterans[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        ArmedForcesVeterans[4].append(current_application_id)
+
+                elif current_application_category == "First time applicants":
+                    if current_application_band == "Band 1":
+                        FirstTimeApplicant[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        FirstTimeApplicant[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        FirstTimeApplicant[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        FirstTimeApplicant[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        FirstTimeApplicant[4].append(current_application_id)
+
+                elif current_application_category == "Grypsy & Travellers":
+                    if current_application_band == "Band 1":
+                        GypsyAndTravellers[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        GypsyAndTravellers[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        GypsyAndTravellers[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        GypsyAndTravellers[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        GypsyAndTravellers[4].append(current_application_id)
+
+                elif current_application_category == "home scheme":
+                    if current_application_band == "Band 1":
+                        homescheme[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        homescheme[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        homescheme[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        homescheme[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        homescheme[4].append(current_application_id)
+                        
+                elif current_application_category == "Homeless":
+                    if current_application_band == "Band 1":
+                        Homeless[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        Homeless[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        Homeless[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        Homeless[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        Homeless[4].append(current_application_id)
+
+                elif current_application_category == "Mobility Schemes (Pan London)":
+                    if current_application_band == "Band 1":
+                        MobilitySchemes[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        MobilitySchemes[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        MobilitySchemes[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        MobilitySchemes[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        MobilitySchemes[4].append(current_application_id)
+
+                elif current_application_category == "Panel moves":
+                    if current_application_band == "Band 1":
+                        MobilitySchemes[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        MobilitySchemes[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        MobilitySchemes[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        MobilitySchemes[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        MobilitySchemes[4].append(current_application_id)
+
+                elif current_application_category == "Panel moves":
+                    if current_application_band == "Band 1":
+                        PanelMoves[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        PanelMoves[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        PanelMoves[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        PanelMoves[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        PanelMoves[4].append(current_application_id)
+                
+                elif current_application_category == "Permanent Decants":
+                    if current_application_band == "Band 1":
+                        PermanentDecants[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        PermanentDecants[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        PermanentDecants[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        PermanentDecants[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        PermanentDecants[4].append(current_application_id)
+
+                elif current_application_category == "Sheltered":
+                    if current_application_band == "Band 1":
+                        Sheltered[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        Sheltered[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        Sheltered[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        Sheltered[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        Sheltered[4].append(current_application_id)
+
+                elif current_application_category == "Social Services Quota (adult)":
+                    if current_application_band == "Band 1":
+                        SocialServicesQuota[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        SocialServicesQuota[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        SocialServicesQuota[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        SocialServicesQuota[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        SocialServicesQuota[4].append(current_application_id)
+
+                elif current_application_category == "Social Services Quota (Children)":
+                    if current_application_band == "Band 1":
+                        SocialServicesQuota[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        SocialServicesQuota[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        SocialServicesQuota[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        SocialServicesQuota[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        SocialServicesQuota[4].append(current_application_id)
+
+                elif current_application_category == "Spare Room downsizer":
+                    if current_application_band == "Band 1":
+                        SpareRoomDownsizer[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        SpareRoomDownsizer[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        SpareRoomDownsizer[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        SpareRoomDownsizer[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        SpareRoomDownsizer[4].append(current_application_id)
+
+                elif current_application_category == "Staff rehousing":
+                    if current_application_band == "Band 1":
+                        StaffRehousing[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        StaffRehousing[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        StaffRehousing[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        StaffRehousing[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        StaffRehousing[4].append(current_application_id)
+
+                elif current_application_category == "Temporary Decants":
+                    if current_application_band == "Band 1":
+                        TemporaryDecants[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        TemporaryDecants[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        TemporaryDecants[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        TemporaryDecants[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        TemporaryDecants[4].append(current_application_id)
+
+                elif current_application_category == "Tenant Finder Service (TFS) Prevention":
+                    if current_application_band == "Band 1":
+                        TenantFinderServicePrevention[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        TenantFinderServicePrevention[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        TenantFinderServicePrevention[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        TenantFinderServicePrevention[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        TenantFinderServicePrevention[4].append(current_application_id)
+
+                elif current_application_category == "Transfer":
+                    if current_application_band == "Band 1":
+                        Transfer[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        Transfer[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        Transfer[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        Transfer[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        Transfer[4].append(current_application_id)
+
+                elif current_application_category == "Under occupier":
+                    if current_application_band == "Band 1":
+                        UnderOccupier[0].append(current_application_id)
+                    elif current_application_band == "Band 2":
+                        UnderOccupier[1].append(current_application_id)
+                    elif current_application_band == "Band 3":
+                        UnderOccupier[2].append(current_application_id)
+                    elif current_application_band == "Band 4":
+                        UnderOccupier[3].append(current_application_id)
+                    elif current_application_band == "Band 5":
+                        UnderOccupier[4].append(current_application_id)
+
+
+            else: 
+                break
+        
+        '''
+        Randomly release properties each day
+        1. Keep track of expected number of property per year
+        2. If that number has not been reached, generate a random number 
+        '''
+
+
+        
+        # print(current_application_date)
+        # print(index)
+        
         current_date = current_date + datetime.timedelta(days=1)
-        print(current_date)
+        # print(current_date)
+
 
