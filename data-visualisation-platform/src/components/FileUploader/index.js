808179de73eb12d4c8f4ad96d1b46d1105f8a8c5
Manish Sudhir <manish@Manishs-MacBook-Pro.local>
2023-02-09 20:07:44 +0000

    implemented s3 bucket upload button w dependencies

--- data-visualisation-platform/src/components/FileUploader/index.js
+++ data-visualisation-platform/src/components/FileUploader/index.js
@@ -1,15 +1,54 @@
 import './style.css';
 
-export const FileUploader = ({}) => {
-    return (
-        <form method="post" action="#" id="#">
-            <div className="form-group files">
-                <label>
-                    Upload Your File
-                <input type="file" className="form-control" multiple=""/>
-                </label>
-            </div>
-            <button> Submit</button>
-        </form>
-    )
-};
+// export const FileUploader = ({}) => {
+//     return (
+//         <form method="post" action="#" id="#">
+//             <div className="form-group files">
+//                 <label>
+//                     Upload Your File
+//                 <input type="file" className="form-control" multiple=""/>
+//                 </label>
+//             </div>
+//             <button> Submit</button>
+//         </form>
+//     )
+// };
+
+import React , {useState} from 'react';
+import { uploadFile } from 'react-s3';
+
+
+const S3_BUCKET ='rbkhousingbucket';
+const REGION ='eu-west-2';
+const ACCESS_KEY ='AKIAX25LW44KLU6GEL7K';
+const SECRET_ACCESS_KEY ='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';
+
+const config = {
+    bucketName: S3_BUCKET,
+    region: REGION,
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+}
+
+const UploadImageToS3WithReactS3 = () => {
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileInput = (e) => {
+        setSelectedFile(e.target.files[0]);
+    }
+
+    const handleUpload = async (file) => {
+        uploadFile(file, config)
+            .then(data => console.log(data))
+            .catch(err => console.error(err))
+    }
+
+    return <div>
+        <div>Data Sheet</div>
+        <input type="file" onChange={handleFileInput}/>
+        <button onClick={() => handleUpload(selectedFile)}>Upload File</button>
+    </div>
+}
+
+export default UploadImageToS3WithReactS3;
