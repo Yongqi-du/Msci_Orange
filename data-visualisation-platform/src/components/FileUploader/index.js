6b21fe4b10d040ce1371b90db235db928185974c
Sebastian Mesca <sebastianmesca@Sebastians-MacBook-Pro.local>
2023-02-09 11:27:32 +0000

    server

--- data-visualisation-platform/src/components/FileUploader/index.js
+++ data-visualisation-platform/src/components/FileUploader/index.js
@@ -1,4 +1,21 @@
 import './style.css';
+import {read, utils, writeFile} from 'xlsx';
+import * as XLSX from 'xlsx'
+const readUploadFile = (e) => {
+    e.preventDefault();
+    if (e.target.files) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const data = e.target.result;
+            const workbook = XLSX.read(data, {type: "array"});
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            const json = XLSX.utils.sheet_to_json(worksheet);
+            console.log(json);
+        };
+       reader.readAsArrayBuffer(e.target.files[0]);
+    }
+}
 
 export const FileUploader = ({}) => {
     return (
@@ -6,7 +23,7 @@ export const FileUploader = ({}) => {
             <div className="form-group files">
                 <label>
                     Upload Your File
-                <input type="file" className="form-control" multiple=""/>
+                <input id="inp" type="file" className="form-control" multiple="" onChange={readUploadFile}/>
                 </label>
             </div>
             <button> Submit</button>
