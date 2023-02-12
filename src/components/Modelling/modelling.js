dd2ab69b88872901b3342216b3197bbf58cf7c91
Guangzhong Chen <guangzhong0@gmail.com>
2023-02-12 18:26:16 +0000

    Added sketches

--- src/components/Modelling/modelling.js
+++ src/components/Modelling/modelling.js
@@ -1,10 +1,17 @@
 import './style.css';
 
+import charts from './group project-6.png';
+import edits from './group project-9.png';
+
 function Modelling() {
     return (
         <div className='flex-col'>
             <div className='h4'>
                 Allocation Policy Modelling
+            </div>
+            <div className='flex-row'>
+                <img src={charts} alt="Charts"/>
+                <img src={edits} alt="Edit Boxes"/>
             </div>
             <button>Submit</button>
         </div>
