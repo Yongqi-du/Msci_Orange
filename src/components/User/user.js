78619b70426632f7deb9686397562191f2135a05
Guangzhong Chen <guangzhong0@gmail.com>
2023-03-07 15:41:30 +0000

    added sign out button

--- src/components/User/user.js
+++ src/components/User/user.js
@@ -1,14 +1,21 @@
-import './style.css';
+import React from 'react';
+import { Auth } from 'aws-amplify';
 
 function User() {
-    return (
-        <div className='flex-col'>
-            <div className='h4'>
-                User
-            </div>
-            <button>Save</button>
-        </div>
-    )
-};
+  const handleSignOut = async () => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log('Error signing out:', error);
+    }
+  };
+
+  return (
+    <div className='flex-col'>
+      <div className='h4'>User</div>
+      <button onClick={handleSignOut}>Sign Out</button>
+    </div>
+  );
+}
 
 export default User;
