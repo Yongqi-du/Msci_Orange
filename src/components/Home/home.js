1dcf5cef33d40c67836f4a1a55c145f450e84f34
Guangzhong Chen <guangzhong0@gmail.com>
2023-03-15 11:25:56 +0000

    added more charts to the home page for demonstration purpose

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -3,28 +3,81 @@ import ReactEcharts from 'echarts-for-react';
 
 const Home = () => {
 
-    const option = {
+    const bandChart = {
         title: {
-            text: '',
+            text: 'Distribution of Band in Applications',
         },
         tooltip: {},
         xAxis: {
-            data: ['Band1', 'Band2', 'Band3', 'Band4', 'Ban5',]
+            data: ['Band 1', 'Band 2', 'Band 3', 'Band 4', 'Band 5',]
         },
         yAxis: {},
         series: [
             {
                 name: '',
                 type: 'bar',
-                data: [5, 20, 36, 10, 10, 20, 10],
+                data: [238, 378, 955, 1377, 205],
+                color: '#2f7ed8' // Set color to blue
             },
         ],
     };
+    
+    const bedroomChart = {
+        title: {
+            text: 'Distribution of Bedroom in Applications',
+        },
+        tooltip: {},
+        xAxis: {
+            data: ['1 Bed', '2 Bed', '3 Bed', '4 Bed', '5 Bed', '6 Bed', '7 Bed']
+        },
+        yAxis: {},
+        series: [
+            {
+                name: '',
+                type: 'bar',
+                data: [1020, 988, 853, 245, 39, 4, 1],
+                color: '#8bbc21' // Set color to green
+            },
+        ],
+    };
+    
+    const yearChart = {
+        title: {
+            text: 'Distribution of Applications over the Years',
+        },
+        tooltip: {},
+        xAxis: {
+            data: [
+                '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002',
+                '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010',
+                '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018',
+                '2019', '2020', '2021', '2022'
+            ]
+        },
+        yAxis: {},
+        series: [
+            {
+                name: '',
+                type: 'bar',
+                data: [
+                    1, 19, 9, 11, 9, 12, 11, 12, 19, 24, 20, 31, 30, 39, 38, 40,
+                    38, 52, 90, 131, 206, 263, 390, 310, 309, 410, 384, 248
+                ],
+                color: '#910000' // Set color to red
+            },
+        ],
+    };
+    
+    
+
+
     const avatarUrl = "logo.png"
 
     return (
         <>
-            <ReactEcharts option={option}/>
+            <ReactEcharts option={bandChart}/>
+            <ReactEcharts option={bedroomChart}/>
+            <ReactEcharts option={yearChart}/>
         </>
     );
 
