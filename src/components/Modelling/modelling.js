48deea02bc8e416137fb49fffaba76e5ce546f89
Guangzhong Chen <guangzhong0@gmail.com>
2023-03-15 09:47:59 +0000

    Feat/policy form (#15)
    * Move work to new UI
    
    * Add json object for submission
    
    * Change View object
    
    * Try lambda implementation
    
    * Add API to lambda func
    
    * Fix lambda
    
    * Add some config files
    
    * Fix file paths for modeller
    
    * Change import paths in modeller
    
    * Change modeller paths again
    
    * Change one more import path
    
    * Remove API call
    
    * Scrap API and Lambda adds for user testing
    
    ---------
    
    Co-authored-by: Fahim Mohammed <mohammedf2606@gmail.com>

--- src/components/Modelling/modelling.js
+++ src/components/Modelling/modelling.js
@@ -1,6 +1,57 @@
-import React, { Component } from 'react';
+import React, {Component, useState} from 'react';
 import ReactEcharts from 'echarts-for-react';
+import {View} from "@aws-amplify/ui-react";
+import {API} from "aws-amplify";
 
+const policyDefaults = {
+  "PanelMoves": 0.02,
+  "Homeless": 0.04,
+  "SocialServicesQuota": 0.04,
+  "Transfer": 0.01,
+  "HomeScheme": 0.04,
+  "FirstTimeApplicants": 0.01,
+  "TenantFinder":  0.01,
+  "Downsizer": 0.02,
+  "Decants": 0.8,
+  "Other": 0.01
+};
+
+const policyLabelNames = {
+  "PanelMoves": "Panel Moves",
+  "Homeless": "Homeless",
+  "SocialServicesQuota": "Social Services Quota",
+  "Transfer": "Transfer",
+  "HomeScheme": "Home Scheme",
+  "FirstTimeApplicants": "First Time Applicants",
+  "TenantFinder": "Tenant Finder",
+  "Downsizer": "Downsizer",
+  "Decants": "Decants",
+  "Other": "Other"
+};
+
+const supplyDefaults = {
+  "1": 58,
+  "2": 53,
+  "3": 29,
+  "4": 2
+};
+
+const supplyLabelNames = {
+  "1": "One Bedroom Properties",
+  "2": "Two Bedroom Properties",
+  "3": "Three Bedroom Properties",
+  "4": "Four or More Bedroom Properties"
+}
+
+const dateDefaults = {
+  "startDate": "2022-01-01",
+  "endDate": "2022-12-31"
+}
+
+const dateLabelNames = {
+  "startDate": "Starting Date",
+  "endDate": "End Date"
+}
 
 const Modelling = () => {
     const option = {
@@ -77,10 +128,124 @@ const Modelling = () => {
         ]
       };
     return (
-        <>
-            <ReactEcharts option={option}/>
-        </>
+      <View>
+        <ReactEcharts option={option}/>
+        <PolicyForm/>
+      </View>
     );
+}
+
+function PolicyForm() {
+  const [policyInputs, setPolicyInputs] = useState(Object.values(policyDefaults));
+  const [supplyInputs, setSupplyInputs] = useState(Object.values(supplyDefaults));
+  const [dateInputs, setDateInputs] = useState(Object.values(dateDefaults));
+
+  const handlePolicyInputChange = (index, value) => {
+    const newPolicyInputs = [...policyInputs];
+    newPolicyInputs[index] = parseFloat(value);
+    setPolicyInputs(newPolicyInputs);
+    policyDefaults[index] = newPolicyInputs[index];
+  };
+
+  const handleSupplyInputChange = (index, value) => {
+    const newSupplyInputs = [...supplyInputs];
+    newSupplyInputs[index] = parseInt(value);
+    setSupplyInputs(newSupplyInputs);
+    supplyDefaults[index] = newSupplyInputs[index];
+  };
+
+  const handleDateInputChange = (index, value) => {
+    const newDateInputs = [...dateInputs];
+    newDateInputs[index] = value;
+    setDateInputs(newDateInputs);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let outputObj = {
+      "policy": {},
+      "supply": {},
+      "startDate": "",
+      "endDate": ""
+    };
+
+    outputObj["policy"] = policyDefaults;
+    outputObj["supply"] = supplyDefaults;
+
+    outputObj["startDate"] = dateInputs[0];
+    outputObj["endDate"] = dateInputs[1];
+
+    // try {
+    //   const response = await API.post(
+    //     "restapimodeller",
+    //     "/modeller-api",
+    //     {
+    //       body: {outputObj}
+    //     }
+    //   );
+    //   console.log(response);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    console.log(outputObj);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Application Policy Inputs</h2>
+      {Object.entries(policyDefaults).map(([key, value], index) => (
+        <div key={index}>
+          <label htmlFor={`policy-input-${index}`}>{policyLabelNames[key]}</label>
+          <br/>
+          <input
+            id={`policy-input-${index}`}
+            type="range"
+            step="0.01"
+            max="1"
+            min="0"
+            name={`policy-input-${index}`}
+            value={policyInputs[index] || value}
+            onChange={(e) => handlePolicyInputChange(index, e.target.value)}
+          />
+          <label htmlFor={`policy-input-${index}`}>{policyInputs[index] || value}</label>
+        </div>
+      ))}
+
+      <h2>Property Supply Inputs</h2>
+      {Object.entries(supplyDefaults).map(([key, value], index) => (
+        <div key={index}>
+          <label htmlFor={`supply-input-${index}`}>{supplyLabelNames[key]}</label>
+          <br/>
+          <input
+            id={`supply-input-${index}`}
+            type="number"
+            step="1"
+            name={`supply-input-${index}`}
+            value={supplyInputs[index] || value}
+            onChange={(e) => handleSupplyInputChange(index, e.target.value)}
+          />
+        </div>
+
+      ))}
+
+      <h2>Date Inputs</h2>
+      {Object.entries(dateDefaults).map(([key,  value], index) => (
+        <div key={index}>
+          <label htmlFor={`date-input-${index}`}>{dateLabelNames[key]}</label>
+          <br/>
+          <input
+            name={`date-input-${index}`}
+            id={`date-input-${index}`}
+            type="date"
+            value={dateInputs[index] || value}
+            onChange={(e) => handleDateInputChange(index, e.target.value)}
+          />
+        </div>
+      ))}
+      <br/>
+      <button type="submit" id="policy-form-submit">Submit</button>
+    </form>
+  );
 }
 
 export default Modelling;
